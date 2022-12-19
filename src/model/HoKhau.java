42d82a208f730d03d0cfcc4f0cade3cc094c0ff0
loi vu <loi07062001@gmail.com>
2022-12-19 15:25:17 +0700

    cap nhap ho khau nhan khau

--- src/model/HoKhau.java
+++ src/model/HoKhau.java
@@ -1,18 +1,22 @@
 package model;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class HoKhau {
     private SimpleStringProperty idHoKhau;
     private SimpleStringProperty tenChuHo;
     private SimpleStringProperty diaChi;
-    private SimpleStringProperty soLuongNhanKhau;
+    private SimpleIntegerProperty soLuongNhanKhau;
      // khoi tao ho khau
-    public HoKhau(SimpleStringProperty idHoKhau, SimpleStringProperty tenChuHo, SimpleStringProperty diaChi, SimpleStringProperty soLuongNhanKhau) {
-        this.idHoKhau = idHoKhau;
-        this.tenChuHo = tenChuHo;
-        this.diaChi = diaChi;
-        this.soLuongNhanKhau = soLuongNhanKhau;
+    public HoKhau() {
+
+    }
+    public HoKhau(String idHoKhau, String tenChuHo, String diaChi, int soLuongNhanKhau) {
+        this.idHoKhau = new SimpleStringProperty(idHoKhau);
+        this.tenChuHo = new SimpleStringProperty(diaChi);
+        this.diaChi = new SimpleStringProperty(diaChi);
+        this.soLuongNhanKhau = new SimpleIntegerProperty(soLuongNhanKhau);
     }
     //phuong thuc getter và setter tuong ung
 
@@ -52,15 +56,15 @@ public class HoKhau {
         this.diaChi.set(diaChi);
     }
 
-    public String getSoLuongNhanKhau() {
+    public int getSoLuongNhanKhau() {
         return soLuongNhanKhau.get();
     }
 
-    public SimpleStringProperty soLuongNhanKhauProperty() {
+    public SimpleIntegerProperty soLuongNhanKhauProperty() {
         return soLuongNhanKhau;
     }
 
-    public void setSoLuongNhanKhau(String soLuongNhanKhau) {
+    public void setSoLuongNhanKhau(int soLuongNhanKhau) {
         this.soLuongNhanKhau.set(soLuongNhanKhau);
     }
 }
